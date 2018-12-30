c8e40b4714bb352a0569003050ccffd7d15aa289
Heikki Pora <heikki.pora@gmail.com>
2018-12-30 18:02:20 +0200

    Refactor

--- src/index.js
+++ src/index.js
@@ -17,35 +17,39 @@ const readdirAsync = Promise.promisify(fs.readdir)
 
 const fileNameRegex = /^[\d]{4}-[\d]{2}-[\d]{2}.*\.pdf$/
 readdirAsync(program.path)
-  .then(fileNames => fileNames.filter(fileName => fileNameRegex.test(fileName))
-                              .map(splitFilename)
-  )
-  .then(rows => ({
+  .then(fileNames => fileNames.filter(fileName => fileNameRegex.test(fileName)).map(splitFilename))
+  .then(splitIncomeAndExpense)
+  .then(calculateTotals)
+  .then(console.log)
+
+function splitIncomeAndExpense(rows) {
+  return {
     income: rows.filter(row => row.price > 0),
-    expense: rows.filter(row => row.price < 0)
-  }))
-  .then(({income, expense}) => {
-    const incomeTotal = income.reduce(accPrice, {total: 0, tax: 0})
-    const expenseTotal = expense.reduce(accPrice, {total: 0, tax: 0})
-    return {
-      income: {
-        totalNoVat: toEuros(incomeTotal.total),
-        vat: toEuros(incomeTotal.tax),
-        total: toEuros(incomeTotal.total + incomeTotal.tax),
-        rows: income
-      },
-      expenses: {
-        totalNoVat: toEuros(expenseTotal.total),
-        vat: toEuros(expenseTotal.tax),
-        total: toEuros(expenseTotal.total - expenseTotal.tax),
-        rows: expense
-      },
-      totalNoVat: toEuros(incomeTotal.total + expenseTotal.total),
-      vat: toEuros(incomeTotal.tax - expenseTotal.tax),
-      total: toEuros(incomeTotal.total + incomeTotal.tax + expenseTotal.total - expenseTotal.tax)
-    }
-  })
-  .then(results => console.log(results))
+    expense: rows.filter(row => row.price < 0)  
+  }
+}
+
+function calculateTotals({income, expense}) {
+  const incomeTotal = income.reduce(accPrice, {total: 0, tax: 0})
+  const expenseTotal = expense.reduce(accPrice, {total: 0, tax: 0})
+  return {
+    income: {
+      totalNoVat: toEuros(incomeTotal.total),
+      vat: toEuros(incomeTotal.tax),
+      total: toEuros(incomeTotal.total + incomeTotal.tax),
+      rows: income
+    },
+    expenses: {
+      totalNoVat: toEuros(expenseTotal.total),
+      vat: toEuros(expenseTotal.tax),
+      total: toEuros(expenseTotal.total - expenseTotal.tax),
+      rows: expense
+    },
+    totalNoVat: toEuros(incomeTotal.total + expenseTotal.total),
+    vat: toEuros(incomeTotal.tax - expenseTotal.tax),
+    total: toEuros(incomeTotal.total + incomeTotal.tax + expenseTotal.total - expenseTotal.tax)
+  }
+}
 
 function splitFilename(fileName) {
   const [datePart, namePart, pricePart, taxPart] = fileName.split('|')
